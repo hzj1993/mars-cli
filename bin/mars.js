5ad22e7c8ea9f340456bf37d64a9a6c61de4bd56
jjaimm <sdad288677777@163.com>
2020-09-11 21:32:43 +0800

    add: init and push

--- bin/mars.js
+++ bin/mars.js
@@ -6,8 +6,18 @@ program.version(require('../package.json').version)
 program
   .command('init <name>')
   .description('init project')
-  .action(name => {
-    console.log('name', name)
+  .action(require('../lib/cli-init/index'))
+
+program
+  .command('push')
+  .description('git commit and push')
+  .option('--amend', 'git add all files but ignore .idea/ and commit amend and push the project')
+  .action((cmd) => {
+    require('../lib/outdated')(cleanArgs(cmd))
   })
 
 program.parse(process.argv)
+
+if (!process.argv.slice(2).length) {
+  program.outputHelp()
+}
