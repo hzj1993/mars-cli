460b0bc9ea243d770dac15ae882d011d70d8e5e7
jjaimm <sdad288677777@163.com>
2020-09-25 18:27:19 +0800

    fix: 修复热更新问题

--- test/dev-template/src/App.vue
+++ test/dev-template/src/App.vue
@@ -1,15 +1,10 @@
 <template>
   <div v-if="state.name">
     <div v-for="i in 10">456</div>
-    <div>321</div>
-    <div>321</div>
-    <div>321</div>
-    <div>321</div>
-    <div>31</div>
-    <div>321</div>
-    <div>321</div>
-    <div>321</div>
-    <div>321</div>
+    <div>789</div>
+    <div>789</div>
+    <div>789</div>
+    <div>000</div>
     <div>{{state.a}}</div>
     <div :id="state.id" :class="state.class">321</div>
     <button @click="increment">click</button>
